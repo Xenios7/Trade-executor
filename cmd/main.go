7420825d8d56ef1d37e5a67d43c327d68c7051eb
Xenios7 <xyerol01@ucy.ac.cy>
2026-07-01 13:38:43 +0300

    M2 complete: Kafka producer publishing trade orders to trade-orders topic

--- cmd/main.go
+++ cmd/main.go
@@ -5,11 +5,22 @@ import (
 	"net/http"
 
 	"github.com/Xenios7/Trade-executor/internal/api"
+	"github.com/Xenios7/Trade-executor/internal/kafka"
 	"github.com/Xenios7/Trade-executor/internal/service"
+	ckafka "github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
 func main() {
-	svc := service.NewOrderService(nil, nil)
+
+	p, err := ckafka.NewProducer(&ckafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	producer := kafka.NewKafkaProducer(p, "trade-orders")
+	svc := service.NewOrderService(producer, nil)
 	h := api.NewHandler(svc)
 	r := api.NewRouter(h)
 
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/Xenios7/Trade-executor
 go 1.26.3
 
 require (
-	github.com/confluentinc/confluent-kafka-go/v2 v2.15.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
+	github.com/confluentinc/confluent-kafka-go/v2 v2.15.0
+	github.com/google/uuid v1.6.0
 )
